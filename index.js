45a0e4c5cc836c1293594b7dd785a1933b068cda
Devanshi Patel <devanshipatel2206@gmail.com>
2022-09-06 12:57:31 +0530

    .

---
+++ index.js
@@ -0,0 +1,9 @@
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".navbar");
+
+const toggleNavbar = () => {
+  nav_header.classList.toggle("active");
+};
+
+mobile_nav.addEventListener("click", () => toggleNavbar());
+
